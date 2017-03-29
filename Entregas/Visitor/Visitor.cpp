b840d73bad4a3a28275c201ec25fdf7fa2762667
iTubePod <hmgc_mauro@icloud.com>
2017-03-28 21:10:52 -0600

    This implements the visitor design pattern with singleton inherited to visitor subclasses

--- Entregas/Visitor/Visitor.cpp
+++ Entregas/Visitor/Visitor.cpp
@@ -1,14 +1,23 @@
+//
+//  Visitor.cpp
+//
+//  Created by Mauricio G. Coello on 22/3/17.
+//  Copyright © 2017 Mauricio G. Coello. All rights reserved.
+//
+// This implements the visitor design pattern with singleton inherited to visitor subclasses
+//
 #include<iostream>
 
 using namespace std;
-
 class Visitor;
 class Number
 {
 protected:
     string quienSoy;
 public:
-    string getQuienSoy(){return quienSoy;}
+    string getQuienSoy(){
+        return quienSoy;
+    }
     virtual void accept(Visitor*) = 0;
 };
 
@@ -35,25 +44,27 @@ public:
 };
 int Double::countDouble = 0;
 
-//template <class T>
 class Visitor
 {
-
 public:
-    virtual void visit(Integer*) = 0;
-    virtual void visit(Double*) = 0;
-    /*T getInstance(){
-        if(this->instance == NULL)
+    template <class T>
+    static T* getInstance()
+    {
+        if(T::instance == NULL)
         {
             T();
+            T* w = new T();
         }
-        else
-            return this->instance;
-    }*/
+        return T::instance;
+    }
+    virtual void visit(Integer*) = 0;
+    virtual void visit(Double*) = 0;
 };
 
 class PresentVisitor : public Visitor
 {
+protected:
+    friend class Visitor;
     PresentVisitor()
     {
         instance = this;
@@ -69,7 +80,7 @@ public:
         else
             return instance;
     }
-
+    
     void visit(Integer* i)
     {
         cout << i->getQuienSoy();
@@ -83,6 +94,13 @@ PresentVisitor* PresentVisitor::instance = 0;
 
 class CountVisitor : public Visitor
 {
+protected:
+    friend class Visitor;
+    CountVisitor()
+    {
+        instance = this;
+    }
+    static CountVisitor* instance;
 public:
     void visit(Integer* i)
     {
@@ -93,9 +111,17 @@ public:
         cout << Double::countDouble;
     }
 };
+CountVisitor* CountVisitor::instance = 0;
 
 class AddVisitor : public Visitor
 {
+protected:
+    friend class Visitor;
+    AddVisitor()
+    {
+        instance = this;
+    }
+    static AddVisitor* instance;
 public:
     void visit(Integer*)
     {
@@ -106,12 +132,11 @@ public:
         Double::countDouble++;
     }
 };
-
+AddVisitor* AddVisitor::instance = 0;
 void Double::accept(Visitor* v)
 {
     v->visit(this);
 }
-
 void Integer::accept(Visitor* v)
 {
     v->visit(this);
@@ -119,24 +144,23 @@ void Integer::accept(Visitor* v)
 
 int main()
 {
-    //Visitor<AddVisitor> v;
-    AddVisitor a;
-    CountVisitor c;
-    PresentVisitor* p = PresentVisitor::getInstance();
+    AddVisitor* a = Visitor::getInstance<AddVisitor>();
+    CountVisitor* c = Visitor::getInstance<CountVisitor>();
+    PresentVisitor* p = Visitor::getInstance<PresentVisitor>();
     
     Number* n[] ={new Integer, new Double};
     
     for(int i=0; i< 5; i++)
     {
-        for (int j = 0; j < 2; j++) 
+        for (int j = 0; j < 2; j++)
         {
-            n[j]->accept(&a);    
+            n[j]->accept(a);
         }
     }
     
     n[0]->accept(p);
-    n[0]->accept(&c);
+    n[0]->accept(c);
     cout << endl;
     n[1]->accept(p);
-    n[1]->accept(&c);
+    n[1]->accept(c);
 }
